6bdab3a357f5c005a067eece0776d8cebec72097
Bean <bnasse2000@gmail.com>
2024-05-12 00:12:31 -0500

    Demension working

--- src/main/java/net/hudson/fortcraft/datagen/DataGenerators.java
+++ src/main/java/net/hudson/fortcraft/datagen/DataGenerators.java
@@ -22,7 +22,7 @@ public class DataGenerators {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput));
-        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput));
+        //generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput));
 
         generator.addProvider(event.includeClient(),new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(),new ModItemModelProvider(packOutput, existingFileHelper));
@@ -31,6 +31,6 @@ public class DataGenerators {
                 new ModBlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(),new ModItemTagGenerator(packOutput, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));
 
-        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+        //generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
     }
 }
